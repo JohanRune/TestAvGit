70477062c8736b287554625d61848e0b835b5003
JohanRune <kjgrune@yahoo.se>
2020-09-29 15:09:56 +0200

    Ett test av mig

---
+++ src/TestGit.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Johan Rune
+ * Date: 2020-09-28
+ * Time: 16:00
+ * Project: IntelliJ IDEA
+ * Copyright: MIT
+ */
+public class TestGit {
+   // C:\Users\Lars\Documents\GitHub
+
+}
